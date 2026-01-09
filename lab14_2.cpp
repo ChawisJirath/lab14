db6e996faca553ca4aac46f63c434b5d5f6cfd83
Wia <chawis_jirath@cmu.ac.th>
2026-01-09 16:31:52 +0700

    lab14 2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,5 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool a[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+void inputMatrix(double a[][N]){
+    for(int i=0;i<N;i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j=0;j<N;j++){
+            cin >> a[i][j];
+        }
+        
+    }
+}
 
-//Do not modify source code above this line 
+void findLocalMax(const double a[][N], bool b[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(i==0 || j==0 || i==N-1 || j==N-1){
+                b[i][j] = false;
+            }
+            else{
+                if(a[i][j] >= a[i-1][j] && a[i][j] >= a[i][j+1] && a[i][j] >= a[i][j-1] && a[i][j] >= a[i+1][j]){
+                    b[i][j]=true;
+                }else{
+                    b[i][j]=false;
+                }
+            }
+        }
+    }
+}
